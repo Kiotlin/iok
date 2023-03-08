6f35f85caae5e0c7ea0752ca1259cfd720e8099b
ri1ken <719401217@qq.com>
2023-03-09 00:59:03 +0900

    fix: disable sync formatting

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -121,7 +121,7 @@ M.on_attach = function(client, bufnr)
     vim.api.nvim_create_autocmd("BufWritePre", {
       group = vim.api.nvim_create_augroup("Format", { clear = true }),
       buffer = bufnr,
-      callback = function() vim.lsp.buf.format { async = true } end
+      callback = function() vim.lsp.buf.format() end
     })
   end
 
